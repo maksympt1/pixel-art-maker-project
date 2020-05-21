041f63d035e5b5184a64f3a20d5fa680ad52fcd9
Maks <maksympt1@gmail.com>
2020-05-21 21:43:32 +0100

    javascript

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
-// Select color input
-// Select size input
+function makeGrid(height,width) {
+    const table = document.getElementById("pixelCanvas");
+    let grid = '';
 
-// When size is submitted by the user, call makeGrid()
+    for (let i = 0; i < height; i++) {
+        grid += '<tr class="row-' + i + '">';
+        for (let j = 0; j < width; j++) {
+            grid += '<td class="cell" id="row-' + i + '_cell-' + j + '"></td>';
+        }
+        grid += '</tr>';
+    }
 
-function makeGrid() {
+    table.innerHTML = grid;
 
-// Your code goes here!
+    addClickEventToCells();
 
 }
+
+function formSubmission() {
+    event.preventDefault();
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWidth').value;
+    makeGrid(height, width);
+}
+
+function addClickEventToCells() {
+    const colorPicker = document.getElementById("colorPicker");
+    const cells = document.getElementsByClassName('cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener("click", function (event) {
+            let clickedCell = event.target;
+            clickedCell.style.backgroundColor = colorPicker.value;
+        });
+    }
+}
+
+document.getElementById('sizePicker').onsubmit = function () {
+    formSubmission();
+};
+
+makeGrid(10, 10);
